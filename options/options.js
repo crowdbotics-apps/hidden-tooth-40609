45722a506fddacc19e4a70420016f4583a6cf71e
Mithilesh@crowdbotics.com <Mithilesh@crowdbotics.com>
2023-05-10 23:10:20 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "hidden-tooth-40609",
-  url: "https://hidden-tooth-40609.botics.co",
-  api: "https://hidden-tooth-40609.botics.co/api/v1"
-}
+  "name": "hidden-tooth-40609",
+  "url": "https://hidden-tooth-40609.botics.co",
+  "api": "https://hidden-tooth-40609.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
