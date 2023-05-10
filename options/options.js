80f34456e454f3e7887905ae2bfcc33c1bfd4c40
Crowdbotics <team@crowdbotics.com>
2023-05-10 17:02:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-tooth-40609",
+  url: "https://hidden-tooth-40609.botics.co",
+  api: "https://hidden-tooth-40609.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
